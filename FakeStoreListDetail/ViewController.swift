5effcfb311d0cf95a3f7d930f6ce54ade4906b94
Matteo Casu <matteo.casu91@gmail.com>
2024-09-17 19:20:26 +0200

    Initial Commit

---
+++ FakeStoreListDetail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeStoreListDetail
+//
+//  Created by Matteo Casu on 17/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
